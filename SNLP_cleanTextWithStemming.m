a0ba6cabe3ee09f6034057e4cb4ebc2149dff490
giacomohandjaras <giacomo.handjaras@imtlucca.it>
2022-02-03 18:13:46 +0100

    revision #8

---
+++ SNLP_cleanTextWithStemming.m
@@ -0,0 +1,13 @@
+function cleaned=SNLP_cleanTextWithStemming(raw,stemmed_words,language)
+
+cleaned="";
+words=strsplit(raw,' ');
+words(cellfun(@isempty,words))=[];
+
+[words_stem] = SNLP_stemDictionary(words, language);
+[words_mask] = SNLP_searchWords(words_stem, stemmed_words);
+words(find(words_mask))=[];
+cleaned=strjoin(words);
+
+
+end
